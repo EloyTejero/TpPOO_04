79c1ecc64ad512b904a2144bb6edf07a8d6249f2
EloyTejero <tejero.eloyet36@gmail.com>
2023-05-10 08:07:48 -0300

    Primer commit, creacion de clases vacias

---
+++ Tienda.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TpPOO_04;
+
+/**
+ *
+ * @author ET36
+ */
+public class Tienda {
+    
+}
