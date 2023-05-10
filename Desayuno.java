b685254b5e4c70558aa6ef523539483cb289435b
EloyTejero <tejero.eloyet36@gmail.com>
2023-05-10 08:42:55 -0300

    Class Desayuno (Lorenzo Zanone Lisandro Lobosco)

--- Desayuno.java
+++ Desayuno.java
@@ -9,5 +9,45 @@ package TpPOO_04;
  * @author ET36
  */
 public class Desayuno {
+    String nombre;
+    Double precio;
+    String descripccion;
+
+    public Desayuno(String nombre, Double precio, String descripccion) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripccion = descripccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripccion() {
+        return descripccion;
+    }
+
+    public void setDescripccion(String descripccion) {
+        this.descripccion = descripccion;
+    }
+    
+    
+
+    @Override
+    public String toString() {
+        return "Desayuno{" + "nombre=" + nombre + ", precio=" + precio + ", descripccion=" + descripccion + '}';
+    }
     
 }
